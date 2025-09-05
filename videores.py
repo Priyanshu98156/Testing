b0c0ea37d1713dc6ef126cb3401be9497ce3843a
Priyanshu98156 <109512538+Priyanshu98156@users.noreply.github.com>
2025-09-05 22:52:56 +0530

    push

--- videores.py
+++ videores.py
@@ -1,2 +1,4 @@
 circle    = "https://github.com/Priyanshu98156/videos/raw/main/circle.mp4"
 processor = "https://github.com/Priyanshu98156/videos/raw/main/processor.mp4"
+triangle  = "https://github.com/Priyanshu98156/videos/raw/main/triangle.mp4"
+pattern   = "https://github.com/Priyanshu98156/videos/raw/main/pattern.mp4"
